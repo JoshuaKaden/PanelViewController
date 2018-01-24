ef9479a9dbd377018d6eb5666869ececc4cdbd6b
Joshua Kaden <jkaden@doitt.nyc.gov>
2018-01-24 14:28:34 -0500

    add PaneBehavior

--- PanelViewController/PanelViewController.swift
+++ PanelViewController/PanelViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
+enum PaneState { case closed, open }
+
 class PanelViewController: UIViewController {
+    
+    private(set) var paneState = PaneState.closed
     
     private let mainViewController: UIViewController
     private let panelViewController: UIViewController
